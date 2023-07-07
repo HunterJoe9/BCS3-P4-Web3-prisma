94e62832cc35951204c0d5ae61ca4b0630278e57
sungyoon <crossbone48@gmail.com>
2023-07-07 13:45:26 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.fallback = {
+            encoding : false,
+            bufferutil: false,
+            "utf-8-validate": false,
+        };
+        return config;
+    },
+};
 
 module.exports = nextConfig
